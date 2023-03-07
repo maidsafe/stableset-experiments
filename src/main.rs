8badaff37a9d7a3e6799a50404222c9089f68617
David Rusu <davidrusu.me@gmail.com>
2023-03-07 08:26:43 -0500

    re-enable double spends

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-// mod fake_crypto;
-// mod ledger;
+mod fake_crypto;
+mod ledger;
 mod membership;
 mod stable_set;
 
 use std::{borrow::Cow, collections::BTreeSet, fmt::Debug};
 
-// use ledger::Wallet;
+use ledger::Wallet;
 use membership::Membership;
 use stateright::{
     actor::{model_peers, Actor, ActorModel, ActorModelState, Id, Network, Out},
@@ -18,7 +18,7 @@ const ELDER_COUNT: usize = 3;
 pub struct State {
     pub membership: Membership,
     is_leaving: bool,
-    // pub wallet: Wallet,
+    pub wallet: Wallet,
 }
 
 impl State {
@@ -36,7 +36,7 @@ pub struct Node {
 #[derive(Clone, Eq, Hash, PartialEq)]
 pub enum Msg {
     Membership(membership::Msg),
-    // Wallet(ledger::Msg),
+    Wallet(ledger::Msg),
     StartReissue,
     TriggerLeave,
 }
@@ -45,7 +45,7 @@ impl Debug for Msg {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Msg::Membership(m) => write!(f, "{m:?}"),
-            // Msg::Wallet(m) => write!(f, "{m:?}"),
+            Msg::Wallet(m) => write!(f, "{m:?}"),
             Msg::StartReissue => write!(f, "StartReissue"),
             Msg::TriggerLeave => write!(f, "TriggerLeave"),
         }
@@ -58,11 +58,11 @@ impl From<membership::Msg> for Msg {
     }
 }
 
-// impl From<ledger::Msg> for Msg {
-//     fn from(msg: ledger::Msg) -> Self {
-//         Self::Wallet(msg)
-//     }
-// }
+impl From<ledger::Msg> for Msg {
+    fn from(msg: ledger::Msg) -> Self {
+        Self::Wallet(msg)
+    }
+}
 
 impl Actor for Node {
     type Msg = Msg;
@@ -70,18 +70,19 @@ impl Actor for Node {
 
     fn on_start(&self, id: Id, o: &mut Out<Self>) -> Self::State {
         let membership = Membership::new(&self.genesis_nodes);
-        // let wallet = Wallet::new(&self.genesis_nodes);
+        let wallet = Wallet::new(&self.genesis_nodes);
 
         if !self.genesis_nodes.contains(&id) {
             o.broadcast(&self.genesis_nodes, &membership.req_join(id).into());
         }
 
-        // if !self.genesis_nodes.contains(&id) {
-        //     o.send(id, Msg::StartReissue);
-        // }
+        if !self.genesis_nodes.contains(&id) {
+            o.send(id, Msg::StartReissue);
+        }
 
         State {
-            membership, /* , wallet */
+            membership,
+            wallet,
             is_leaving: false,
         }
     }
@@ -107,23 +108,21 @@ impl Actor for Node {
                     o.send(id, Msg::TriggerLeave);
                 }
             }
-            // Msg::Wallet(msg) => {
-            //     state.to_mut().wallet.on_msg(&elders, id, src, msg, o)
-            // }
+            Msg::Wallet(msg) => state.to_mut().wallet.on_msg(&elders, id, src, msg, o),
             Msg::StartReissue => {
-                //     let input = state.wallet.ledger.genesis_dbc.clone();
+                let input = state.wallet.ledger.genesis_dbc.clone();
 
-                //     let reissue_amount = (0..self.peers.len() + 1)
-                //         .find(|x| Id::from(*x) == id)
-                //         .unwrap() as u64;
-                //     let difference = input.amount() - reissue_amount;
+                let reissue_amount = (0..self.peers.len() + 1)
+                    .find(|x| Id::from(*x) == id)
+                    .unwrap() as u64;
+                let difference = input.amount() - reissue_amount;
 
-                //     state.to_mut().wallet.reissue(
-                //         &elders,
-                //         vec![input],
-                //         vec![reissue_amount, difference],
-                //         o,
-                //     );
+                state.to_mut().wallet.reissue(
+                    &elders,
+                    vec![input],
+                    vec![reissue_amount, difference],
+                    o,
+                );
             }
             Msg::TriggerLeave => {
                 o.broadcast(&elders, &state.to_mut().membership.req_leave(id).into());
@@ -185,11 +184,10 @@ fn prop_all_nodes_who_are_leaving_eventually_left(state: &ActorModelState<Node, 
 }
 
 #[allow(unused)]
-fn prop_unspent_outputs_equals_genesis_amount(_state: &ActorModelState<Node, Vec<Msg>>) -> bool {
-    // state.actor_states.iter().all(|actor| {
-    //     actor.wallet.ledger.genesis_amount() == actor.wallet.ledger.sum_unspent_outputs()
-    // })
-    true
+fn prop_unspent_outputs_equals_genesis_amount(state: &ActorModelState<Node, Vec<Msg>>) -> bool {
+    state.actor_states.iter().all(|actor| {
+        actor.wallet.ledger.genesis_amount() == actor.wallet.ledger.sum_unspent_outputs()
+    })
 }
 
 impl ModelCfg {
@@ -215,26 +213,25 @@ impl ModelCfg {
                 "everyone who started leaving, will leave",
                 |_, state| prop_all_nodes_who_are_leaving_eventually_left(state),
             )
+            .property(Expectation::Always, "Ledger balances", |_, state| {
+                prop_unspent_outputs_equals_genesis_amount(state)
+            })
+            .property(
+                Expectation::Always,
+                "Never two nodes aggregate a double spend",
+                |_, state| {
+                    let concurrent_txs = BTreeSet::from_iter(
+                        state
+                            .actor_states
+                            .iter()
+                            .filter_map(|a| a.wallet.pending_tx.clone())
+                            .filter(|(tx, sig)| sig.verify(&sig.voters, tx))
+                            .map(|(tx, _)| tx),
+                    );
 
-        // .property(Expectation::Always, "Ledger balances", |_, state| {
-        //     prop_unspent_outputs_equals_genesis_amount(state)
-        // })
-        // .property(
-        //     Expectation::Always,
-        //     "Never two nodes aggregate a double spend",
-        //     |_, state| {
-        //         let concurrent_txs = BTreeSet::from_iter(
-        //             state
-        //                 .actor_states
-        //                 .iter()
-        //                 .filter_map(|a| a.wallet.pending_tx.clone())
-        //                 .filter(|(tx, sig)| sig.verify(&sig.voters, tx))
-        //                 .map(|(tx, _)| tx),
-        //         );
-
-        //         concurrent_txs.len() <= 1
-        //     },
-        // )
+                    concurrent_txs.len() <= 1
+                },
+            )
     }
 }
 
@@ -245,7 +242,7 @@ fn main() {
 
     ModelCfg {
         elder_count: 1,
-        server_count: 5,
+        server_count: 4,
         network,
     }
     .into_model()
